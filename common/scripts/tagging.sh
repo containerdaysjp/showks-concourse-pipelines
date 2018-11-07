0fd900ee68346a889daab11fcc37fc44af83ed65
MasayaAoyama <tennis.tenmasa@gmail.com>
2018-11-07 16:38:30 +0900

    [draft] add showk-portal and tagging shell

---
+++ common/scripts/tagging.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+cd ${GITREPO}
+
+STG_COMMIT_HASH=$(git show-branch --merge-base --all)
+MASTER_COMMIT_HASH=$(cat .git/refs/heads/master)
+
+STG_IMAGE_NAME=${IMAGE_NAME}:stg-${STG_COMMIT_HASH}
+MASTER_IMAGE_NAME=${IMAGE_NAME}:prod-${MASTER_COMMIT_HASH}
+
+docker login -u ${REGISTRY_USER} -p ${REGISTRY_PASS}
+docker pull ${STG_IMAGE_NAME}
+docker tag ${STG_IMAGE_NAME} ${MASTER_IMAGE_NAME}
+docker push ${MASTER_IMAGE_NAME}
