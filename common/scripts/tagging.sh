2cd2cb31482edb82077cdc16a9e363dce278d496
MasayaAoyama <tennis.tenmasa@gmail.com>
2018-11-09 18:00:17 +0900

    modify for tagging

--- common/scripts/tagging.sh
+++ common/scripts/tagging.sh
@@ -8,7 +8,11 @@ MASTER_COMMIT_HASH=$(cat .git/refs/heads/master)
 STG_IMAGE_NAME=${IMAGE_NAME}:stg-${STG_COMMIT_HASH}
 MASTER_IMAGE_NAME=${IMAGE_NAME}:prod-${MASTER_COMMIT_HASH}
 
-docker login -u ${REGISTRY_USER} -p ${REGISTRY_PASS}
-docker pull ${STG_IMAGE_NAME}
-docker tag ${STG_IMAGE_NAME} ${MASTER_IMAGE_NAME}
-docker push ${MASTER_IMAGE_NAME}
+git clone git@github.com:MasayaAoyama/moby.git
+cd moby
+sh download-frozen-image-v2.sh ${STG_IMAGE_NAME}
+mv ${STG_IMAGE_NAME}.tgz ../image.tgz
+
+#docker login -u ${REGISTRY_USER} -p ${REGISTRY_PASS}
+#docker tag ${STG_IMAGE_NAME} ${MASTER_IMAGE_NAME}
+#docker push ${MASTER_IMAGE_NAME}
