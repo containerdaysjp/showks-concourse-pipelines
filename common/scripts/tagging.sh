6ca28d49d72c7197098d5b6e06493a8a3f833b69
MasayaAoyama <tennis.tenmasa@gmail.com>
2018-11-11 08:30:37 +0900

    add for canvas template and portal

--- common/scripts/tagging.sh
+++ common/scripts/tagging.sh
@@ -4,22 +4,23 @@ cd ${GITREPO}
 git config remote.origin.fetch "+refs/heads/*:refs/remotes/origin/*"
 git fetch --all
 
-git config --global pager.branch cat
-git config --global pager.log cat
-git branch --all -v
-echo ===============
-git branch
-echo ============
-git branch --all
-echo =================
-git log --graph --color --date-order -C -M --all --date=short --pretty=format:"<%h> %ad [%an] %Cgreen%d%Creset %s"
-echo =================
+# git config --global pager.branch cat
+# git config --global pager.log cat
+# git branch --all -v
+# echo ===============
+# git branch
+# echo ============
+# git branch --all
+# echo =================
+# git log --graph --color --date-order -C -M --all --date=short --pretty=format:"<%h> %ad [%an] %Cgreen%d%Creset %s"
+# echo =================
 
 
 STG_COMMIT_HASH=$(git show-branch --merge-base --all)
 MASTER_COMMIT_HASH=$(cat .git/refs/heads/master)
 
 # STG_IMAGE_NAME=${IMAGE_NAME}:stg-${STG_COMMIT_HASH}
+
 STG_IMAGE_NAME=${IMAGE_NAME}:latest
 MASTER_IMAGE_NAME=${IMAGE_NAME}:prod-${MASTER_COMMIT_HASH}
 
@@ -27,11 +28,11 @@ git clone https://github.com/MasayaAoyama/moby.git
 cd moby
 mkdir ../../outfile
 bash download-frozen-image-v2.sh ${STG_IMAGE_NAME}
-ls -lah
+# ls -lah
 mv ${STG_IMAGE_NAME}.tar ../../outfile/image
-cd ../../outfile
-pwd
-ls -lah
+# cd ../../outfile
+# pwd
+# ls -lah
 
 #docker login -u ${REGISTRY_USER} -p ${REGISTRY_PASS}
 #docker tag ${STG_IMAGE_NAME} ${MASTER_IMAGE_NAME}
