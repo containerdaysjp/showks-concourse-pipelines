66f03cd1b962ab44976f0cbeaf097d80e615745a
MasayaAoyama <tennis.tenmasa@gmail.com>
2018-11-11 23:00:12 +0900

    modify for update manigest

---
+++ common/scripts/update_manifest.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo ${GITHUB_KEY} > ~/.ssh/id_rsa
+
+IMAGE_TAG=`cat app-master/.git/refs/heads/${BRANCH}`
+
+git clone -b ${BRANCH} git@github.com:containerdaysjp/${APP_NAME}.git
+cd showks-manifests/
+mkdir -p manifests/${APP_NAME}/
+helm template helm/${APP_NAME} --set image.tag=${IMAGE_TAG} > manifests/${APP_NAME}/manifest.yaml
+git push origin ${BRANCH}
+
