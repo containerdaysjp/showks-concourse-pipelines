369e38ba66e39a164074b6090bd9dac0dc69e8c5
MasayaAoyama <tennis.tenmasa@gmail.com>
2018-11-11 23:28:03 +0900

    add initializing

--- common/scripts/update_manifest.sh
+++ common/scripts/update_manifest.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+mkdir ~/.ssh
+yum -y install git
+cd /tmp
+curl -sL https://storage.googleapis.com/kubernetes-helm/helm-v2.9.1-linux-amd64.tar.gz -o /tmp/helm.tar.gz
+tar -xvf /tmp/helm.tar.gz
+mv linux-amd64/helm /usr/local/bin/
+chmod 755 /usr/local/bin/helm
+
+
 echo ${GITHUB_KEY} > ~/.ssh/id_rsa
 
 IMAGE_TAG=`cat app/.git/refs/heads/${BRANCH}`
 
-git clone -b ${BRANCH} git@github.com:containerdaysjp/${APP_NAME}.git
+git clone -b ${BRANCH} git@github.com:containerdaysjp/showks-manifests.git
 cd showks-manifests/
 mkdir -p manifests/${APP_NAME}/
 helm template helm/${APP_NAME} --set image.tag=${IMAGE_TAG} > manifests/${APP_NAME}/manifest.yaml
