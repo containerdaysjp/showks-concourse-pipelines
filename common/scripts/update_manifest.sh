ca8b7e0b4f1b80c0800c7ba710812989ea4337f3
MasayaAoyama <tennis.tenmasa@gmail.com>
2018-11-13 02:46:13 +0800

    mod

--- common/scripts/update_manifest.sh
+++ common/scripts/update_manifest.sh
@@ -12,7 +12,7 @@ chmod 755 /usr/local/bin/helm
 IMAGE_TAG=${TAG_PREFIX}`git --git-dir ./app/.git rev-parse HEAD`
 BRANCH=`git --git-dir ./k8s-manifests/.git rev-parse --abbrev-ref HEAD`
 
-cp -r k8s-manifests changed-k8s-manifests
+cp -ar k8s-manifests changed-k8s-manifests
 cd changed-k8s-manifests/
 mkdir -p manifests/${APP_NAME}/
 helm template helm/${APP_NAME} --set image.tag=${IMAGE_TAG} > manifests/${APP_NAME}/manifest.yaml
