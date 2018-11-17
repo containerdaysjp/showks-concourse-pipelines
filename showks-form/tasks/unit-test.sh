8aab9fa211b37e1ba9e9854433f68d846d764b38
Kazuto Kusama <jacopen@gmail.com>
2018-11-17 23:45:21 +0900

    Add showks pipeline

---
+++ showks-form/tasks/unit-test.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+inputDir=
+
+while [ $# -gt 0 ]; do
+  case $1 in
+    -i | --input-dir )
+      inputDir=$2
+      shift
+      ;;
+    * )
+      echo "Unrecognized option: $1" 1>&2
+      exit 1
+      ;;
+  esac
+  shift
+done
+
+error_and_exit() {
+  echo $1 >&2
+  exit 1
+}
+
+if [ ! -d "$inputDir" ]; then
+  error_and_exit "missing input directory: $inputDir"
+fi
+
+cd $inputDir
+
+ls
