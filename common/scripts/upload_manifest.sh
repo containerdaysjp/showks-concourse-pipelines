c54efe530724b78781c47ff7f80d2629f5a819c0
Kazuto Kusama <jacopen@gmail.com>
2018-11-27 02:29:42 +0900

    Fix filename

--- common/scripts/upload_manifest.sh
+++ common/scripts/upload_manifest.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 set -e
 
+find .
+
 echo $SERVICE_ACCOUNT > credential.json
 gcloud auth activate-service-account --key-file=credential.json
 gcloud config set project $PROJECT_NAME 
 
-IMAGE_TAG=${TAG_PREFIX}`cat ./app/.git/refs`
+IMAGE_TAG=${TAG_PREFIX}`cat ./app/.git/refs/heads/${BRANCH}`
 
 cp -ar k8s-manifests/* k8s-manifests/.git changed-k8s-manifests/
 cd changed-k8s-manifests/
 mkdir -p manifests/${APP_NAME}/
 helm template ../app/helm/ --set image.tag=${IMAGE_TAG} --set nameSuffix=${NAME_SUFFIX} --set userID=${USERID} --set vhostDomain=${VHOST_DOMAIN} > manifests/${APP_NAME}/manifest.yaml
 
-gsutils manifests/${APP_NAME}/manifest.yaml cp gs://showks-concourse-bucket/manifests${APP_NAME}/manifest.yaml
+gsutil cp manifests/${APP_NAME}/manifest.yaml gs://showks-concourse-bucket/manifests${APP_NAME}/manifest.yaml
