317de872b8bbc81380aa2ade9bac6ba17ad474da
Kazuto Kusama <jacopen@gmail.com>
2018-11-27 11:36:45 +0900

    FIx

--- common/scripts/upload_manifest.sh
+++ common/scripts/upload_manifest.sh
@@ -14,4 +14,4 @@ cd changed-k8s-manifests/
 mkdir -p manifests/${APP_NAME}/
 helm template ../app/helm/ --set image.tag=${IMAGE_TAG} --set nameSuffix=${NAME_SUFFIX} --set userID=${USERID} --set vhostDomain=${VHOST_DOMAIN} > manifests/${APP_NAME}/manifest.yaml
 
-gsutils manifests/${APP_NAME}/manifest.yaml cp gs://showks-concourse-bucket/manifests${APP_NAME}/manifest.yaml
+gsutil manifests/${APP_NAME}/manifest.yaml cp gs://showks-concourse-bucket/manifests${APP_NAME}/manifest.yaml
